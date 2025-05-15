326d0d8767118999ef9357a5e4097be666d1c9bb
GDnickJB29 <jamesblane29@gmail.com>
2025-05-15 00:21:19 -0600

    Prueba_1.0.1

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Laboratorio Virtual</h1>
+    <div id="gameContainer">
+        <iframe id="unityFrame" src="builds_unity/index.html" width="1280" height="720"></iframe>
+    </div>
+</body>
+
+</html>
